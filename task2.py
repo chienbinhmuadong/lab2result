500bfc3aef22977dac4b38b113686d3fc52a40d1
Bui Thanh Dung <buidungitmo2227@gmail.com>
2023-10-20 15:33:47 +0300

    result variant 5

---
+++ task2.py
@@ -0,0 +1,8 @@
+white='\u001b[47m'
+reset='\u001b[0m'
+#for i in range(6):
+print(f"{white}{' '*6}{reset}{' '*6}{white}{' '*6}{reset}")
+print(f"{' '*3}{white}{' '*4}{reset}{' '*4}{white}{' '*4}{reset}")
+print(f"{' '*5}{white}{' '*3}{reset}{' '*2}{white}{' '*3}{reset}")
+print(f"{' '*7}{white}{' '*2}{reset}{' '*0}{white}{' '*2}{reset}")
+print(f"{' '*8}{white}{' '*2}{reset}")
