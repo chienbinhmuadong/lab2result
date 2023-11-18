5bdcb64ff67826f3a20315e34ac0796daf6ff121
Bui Thanh Dung <buidungitmo2227@gmail.com>
2023-11-18 14:55:00 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,10 +1,12 @@
+MIN = -9
+MAX = 9
 red='\u001b[31m'
 reset='\u001b[0m'
 point1=f"{red} +{reset}" 
 point2=f"{red} + {reset}"
-for i in range (10): 
-    if i !=9:
+width_graphic=10
+for i in range (width_graphic): 
+    if i !=MAX:
         print(f"{' . '*i}{point2}{' . '*(9-i-1)}{9-i}{' . '*(9-i-1)}{point1}{'  .'*i}")
     else:
-        print("-9 -8 -7 -6 -5 -4 -3 -2 -1 0 1  2  3  4  5  6  7  8  9")
-         
+        print(" ".join(map(str, range(MIN, 1))), '  '.join(map(str, range(1,MAX+1))))
